08ddbfa163d262290da7880b5908eafbf5c98fe3
Mahdi <mahdisattarnia@hotmail.com>
2024-04-10 18:20:24 +0100

    clubs seeder done, players seeder partially completed

---
+++ config/clubs.js
@@ -0,0 +1,124 @@
+const CLUBS = [
+  {
+    name: "Manchester United F.C.",
+    logo: "logos/manchester_united.png",
+    year_founded: "1878",
+    location: "Old Trafford",
+  },
+  {
+    name: "Manchester City F.C.",
+    logo: "logos/manchester_city.png",
+    year_founded: "1880",
+    location: "Etihad Stadium",
+  },
+  {
+    name: "Liverpool F.C.",
+    logo: "logos/liverpool.png",
+    year_founded: "1892",
+    location: "Anfield",
+  },
+  {
+    name: "Chelsea F.C.",
+    logo: "logos/chelsea.png",
+    year_founded: "1905",
+    location: "Stamford Bridge",
+  },
+  {
+    name: "Tottenham Hotspur F.C.",
+    logo: "logos/tottenham_hotspur.png",
+    year_founded: "1882",
+    location: "Tottenham Hotspur Stadium",
+  },
+  {
+    name: "Arsenal F.C.",
+    logo: "logos/arsenal.png",
+    year_founded: "1886",
+    location: "Emirates Stadium",
+  },
+  {
+    name: "West Ham United F.C.",
+    logo: "logos/west_ham.png",
+    year_founded: "1895",
+    location: "London Stadium",
+  },
+  {
+    name: "Everton F.C.",
+    logo: "logos/everton.png",
+    year_founded: "1878",
+    location: "Goodison Park",
+  },
+  {
+    name: "A.F.C. Bournemouth",
+    logo: "logos/bournemouth.png",
+    year_founded: "1899",
+    location: "Vitality Stadium",
+  },
+  {
+    name: "Wolverhampton Wanderers F.C.",
+    logo: "logos/wolves.png",
+    year_founded: "1877",
+    location: "Molineux Stadium",
+  },
+  {
+    name: "Aston Villa F.C.",
+    logo: "logos/aston_villa.png",
+    year_founded: "1874",
+    location: "Villa Park",
+  },
+  {
+    name: "Newcastle United F.C.",
+    logo: "logos/newcastle_united.png",
+    year_founded: "1892",
+    location: "St. James Park",
+  },
+  {
+    name: "Crystal Palace F.C.",
+    logo: "logos/crystal_palace.png",
+    year_founded: "1905",
+    location: "Selhurst Park",
+  },
+  {
+    name: "Brighton & Hove Albion F.C.",
+    logo: "logos/brighton.png",
+    year_founded: "1901",
+    location: "American Express Stadium",
+  },
+  {
+    name: "Fulham F.C.",
+    logo: "logos/fulham.png",
+    year_founded: "1879",
+    location: "Craven Cottage",
+  },
+  {
+    name: "Brentford F.C.",
+    logo: "logos/brentford.png",
+    year_founded: "1889",
+    location: "Gtech Community Stadium",
+  },
+  {
+    name: "Burnley F.C.",
+    logo: "logos/burnley.png",
+    year_founded: "1882",
+    location: "Turf Moor",
+  },
+  {
+    name: "Nottingham Forest F.C.",
+    logo: "logos/nottingham_forrest.png",
+    year_founded: "1865",
+    location: "City Ground",
+  },
+  {
+    name: "Luton Town F.C.",
+    logo: "logos/luton_town.png",
+    year_founded: "1885",
+    location: "Kenilworth Road",
+  },
+  {
+    name: "Sheffield United F.C.",
+    logo: "logos/sheffield_united.png",
+    year_founded: "1889",
+    location: "Bramall Lane",
+  },
+];
+
+export default CLUBS;
--- config/seeder.js
+++ config/seeder.js
@@ -1,4 +1,6 @@
 import pool from "./pgClient.js";
+import CLUBS from "./clubs.js";
+import PLAYERS from "./players.js";
 
 async function seederDatabase() {
   try {
@@ -22,47 +24,44 @@ async function seederDatabase() {
     await pool.query(`
       CREATE TABLE players (
         PlayerID INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-        First_name VARCHAR (255) NOT NULL,
-        Last_Name VARCHAR (255) NOT NULL,
-        Position VARCHAR (255) NOT NULL,
-        Nationality VARCHAR (255) NOT NULL,
-        Club_ID INT REFERENCES clubs(ClubID)
+        first_name VARCHAR (255) NOT NULL,
+        last_name VARCHAR (255) NOT NULL,
+        position VARCHAR (255) NOT NULL,
+        nationality VARCHAR (255) NOT NULL,
+        team VARCHAR (255) NOT NULL
       );
     `);
     //Create a Manager table
     await pool.query(`
       CREATE TABLE managers (
-        First_name VARCHAR(255),
-        Last_name VARCHAR(255),
-        Nationality VARCHAR (255),
-        Club_ID INT REFERENCES clubs(ClubID)
+        first_name VARCHAR(255),
+        last_name VARCHAR(255),
+        nationality VARCHAR (255),
+        team VARCHAR (255)
       );
     `);
+
     //Seed the clubs table
-    await pool.query(`
-      INSERT INTO clubs (Name, Logo, Year_Founded, Location) 
-      VALUES
-        ('Manchester United F.C.', 'https://example.com/logos/manchester_united.png', '1878', 'Old Trafford'),
-        ('Manchester City F.C.', 'https://example.com/logos/manchester_city.png', '1880', 'Etihad Stadium'),
-        ('Liverpool F.C.', 'https://example.com/logos/liverpool.png', '1892', 'Anfield'),
-        ('Chelsea F.C.', 'https://example.com/logos/chelsea.png', '1905', 'Stamford Bridge'),
-        ('Tottenham Hotspur F.C.', 'https://example.com/logos/tottenham_hotspur.png', '1882', 'Tottenham Hotspur Stadium'),
-        ('Arsenal F.C.', 'https://example.com/logos/arsenal.png', '1886', 'Emirates Stadium'),
-        ('West Ham United F.C.', 'https://example.com/logos/west_ham.png', '1895', 'London Stadium'),
-        ('Everton F.C.', 'https://example.com/logos/everton.png', '1878', 'Goodison Park'),
-        ('A.F.C. Bournemouth', 'https://example.com/logos/bournemouth.png', '1899', 'Vitality Stadium'),
-        ('Wolverhampton Wanderers F.C.', 'https://example.com/logos/wolves.png', '1877', 'Molineux Stadium'),
-        ('Aston Villa F.C.', 'https://example.com/logos/aston_villa.png', '1874', 'Villa Park'),
-        ('Newcastle United F.C.', 'https://example.com/logos/newcastle_united.png', '1892', 'St. James Park'),
-        ('Crystal Palace F.C.', 'https://example.com/logos/crystal_palace.png', '1905', 'Selhurst Park'),
-        ('Brighton & Hove Albion F.C.', 'https://example.com/logos/brighton.png', '1901', 'American Express Stadium'),
-        ('Fulham F.C.', 'https://example.com/logos/fulham.png', '1879', 'Craven Cottage'),
-        ('Brentford F.C.', 'https://example.com/logos/brentford.png', '1889', 'Gtech Community Stadium'),
-        ('Burnley F.C.', 'https://example.com/logos/burnley.png', '1882', 'Turf Moor'),
-        ('Nottingham Forest F.C.', 'https://example.com/logos/nottingham_forrest.png', '1865', 'City Ground'),
-        ('Luton Town F.C.', 'https://example.com/logos/luton_town.png', '1885', 'Kenilworth Road'),
-        ('Sheffield United F.C.', 'https://example.com/logos/sheffield_united.png', '1889', 'Bramall Lane')
-    `);
+    for (const club of CLUBS) {
+      await pool.query(
+        `INSERT INTO clubs(Name, Logo, Year_Founded, Location) values($1, $2, $3, $4)`,
+        [club.name, club.logo, club.year_founded, club.location]
+      );
+    }
+
+    //seed the player table
+    for (const player of PLAYERS) {
+      await pool.query(
+        `INSERT INTO players(first_name, last_name, position, nationality, team) values($1, $2, $3, $4, $5)`,
+        [
+          player.first_name,
+          player.last_name,
+          player.position,
+          player.nationality,
+          player.team,
+        ]
+      );
+    }
   } catch (error) {
     console.log(error);
   }
