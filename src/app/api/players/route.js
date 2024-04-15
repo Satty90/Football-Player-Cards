28b3365129de0680330e53bb294a9133ad770438
Mahdi <mahdisattarnia@hotmail.com>
2024-04-15 14:27:56 +0100

    db completed players api done

--- src/app/api/players/route.js
+++ src/app/api/players/route.js
@@ -1,5 +1,8 @@
 import { NextResponse } from "next/server";
+import pool from "@/app/api/db";
 
 export const GET = async () => {
-  return NextResponse.json({ message: "This is the player api!" });
+  const response = await pool.query("SELECT * FROM players");
+  const { rows } = response;
+  return NextResponse.json(rows);
 };
